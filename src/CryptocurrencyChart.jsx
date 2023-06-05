c65fd3732ea77a1b59fe6a2e1568f72a6f8642fa
daniela <quitiandaniela22@gmail.com>
2023-06-05 16:49:37 -0500

    the project is finished, adding the graphic, and the CSS style

--- src/CryptocurrencyChart.jsx
+++ src/CryptocurrencyChart.jsx
@@ -1,53 +1,54 @@
 import React, { useState, useEffect, useRef } from "react";
 import Chart from "chart.js/auto";
 
-const CryptoCurrency = () => {
-    const [data, setData] = useState(null);
+const CryptoCurrency = ({ data, selectedCrypto }) => {
+    const [chartData, setChartData] = useState(null);
     const chartRef = useRef(null);
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(
-                    "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=30"
-                );
-                const responseData = await response.json();
+        if (data && selectedCrypto) {
+            const fetchData = async () => {
+                try {
+                    const response = await fetch(
+                        `https://api.coingecko.com/api/v3/coins/${selectedCrypto.id}/market_chart?vs_currency=usd&days=30`
+                    );
+                    const responseData = await response.json();
 
-                const dates = [];
-                const prices = [];
+                    const dates = [];
+                    const prices = [];
 
-                const startDate = new Date(responseData.prices[0][0]);
-                for (let i = 0; i < 30; i++) {
-                    const date = new Date(startDate);
-                    date.setDate(startDate.getDate() + i);
-                    dates.push(date.toLocaleDateString());
-                    prices.push(responseData.prices[i][1]);
+                    const startDate = new Date(responseData.prices[0][0]);
+                    for (let i = 0; i < 50; i++) {
+                        const date = new Date(startDate);
+                        dates.push(date.toLocaleDateString());
+                        prices.push(responseData.prices[i][1]);
+                    }
+
+                    const formattedData = {
+                        labels: dates,
+                        datasets: [
+                            {
+                                label: `${selectedCrypto.name} Price (USD)`,
+                                data: prices,
+                                backgroundColor: "rgba(192, 192, 192, 0.8)",
+                                borderWidth: 2,
+                                hoverBackgroundColor: "#C1EE0A",
+                            },
+                        ],
+                    };
+
+                    setChartData(formattedData);
+                } catch (error) {
+                    console.error(error);
                 }
+            };
 
-                const formattedData = {
-                    labels: dates,
-                    datasets: [
-                        {
-                            label: "Bitcoin Price (USD)",
-                            data: prices,
-                            backgroundColor: "rgba(192, 192, 192, 0.8)",
-                            borderWidth: 2,
-                            hoverBackgroundColor: "#C1EE0A",
-                        },
-                    ],
-                };
-
-                setData(formattedData);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        fetchData();
-    }, []);
+            fetchData();
+        }
+    }, [data, selectedCrypto]);
 
     useEffect(() => {
-        if (data) {
+        if (chartData) {
             const ctx = chartRef.current.getContext("2d");
 
             if (chartRef.current.chart) {
@@ -56,7 +57,7 @@ const CryptoCurrency = () => {
 
             chartRef.current.chart = new Chart(ctx, {
                 type: "bar",
-                data: data,
+                data: chartData,
                 options: {
                     responsive: true,
                     maintainAspectRatio: false,
@@ -66,21 +67,20 @@ const CryptoCurrency = () => {
                                 display: false,
                             },
                             ticks: {
+                                display: false,
                                 font: {
                                     size: 12,
                                 },
                             },
-                            categoryPercentage: 1,
-                            barPercentage: 0.8,
                         },
                         y: {
                             grid: {
                                 display: true,
                             },
-                            min: 28000,
-                            max: 30000,
+                            min: Math.min(...chartData.datasets[0].data),
+                            max: Math.max(...chartData.datasets[0].data),
                             ticks: {
-                                stepSize: 500,
+                                stepSize: 300,
                                 font: {
                                     size: 12,
                                 },
@@ -95,11 +95,11 @@ const CryptoCurrency = () => {
                 },
             });
         }
-    }, [data]);
+    }, [chartData]);
 
     return (
-        <div style={{ width: "100%", height: "400px" }}>
-            {data ? <canvas ref={chartRef}></canvas> : <p>Loading...</p>}
+        <div style={{ width: "84%", height: "320px" }}>
+            {chartData ? <canvas ref={chartRef}></canvas> : <p>Loading Data...</p>}
         </div>
     );
 };
--- src/DataCryptos.jsx
+++ src/DataCryptos.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { datesCrypto } from './Helper';
 import './DataCryptos.css';
 import CryptoCurrency from './CryptocurrencyChart';
@@ -6,22 +6,30 @@ import CryptoCurrency from './CryptocurrencyChart';
 const DataCryptos = () => {
     const [data, setData] = useState([]);
     const [searchQuery, setSearchQuery] = useState('');
-
-    useEffect(() => {
-        fetchCrypto();
-    }, []);
+    const [chooseCrypto, setChooseCrypto] = useState(null);
 
     const fetchCrypto = async () => {
         try {
             const jsonData = await datesCrypto();
             setData(jsonData);
+            if (jsonData.length > 0) {
+                setChooseCrypto(jsonData[0]);
+            }
         } catch (error) {
             console.error(error);
         }
     };
 
+    if (data.length === 0) {
+        fetchCrypto();
+    }
+
     const handleSearch = (event) => {
         setSearchQuery(event.target.value);
+    };
+
+    const handleCryptoSelect = (crypto) => {
+        setChooseCrypto(crypto);
     };
 
     const filteredData = data.filter((crypto) => {
@@ -33,10 +41,12 @@ const DataCryptos = () => {
             <div className="overallContainer">
                 <div className="graphicsContainer">
                     <div className="graphicsInitial">
-                        <h1>Sales Activity</h1>
-                        <p>Here you can compare sales channel to determine the most effective</p>
-                        <p>channels and develop a sales strategy based on this data.</p>
-                        <div><CryptoCurrency data={filteredData} /></div>
+                    <h1 style={{ fontWeight: '400' }}>Sales Activity</h1>
+                        <p style={{ fontSize: "13px", color: "#656565" }}>Here you can compare sales channel to determine the most effective</p>
+                        <p style={{ fontSize: "13px", color: "#656565" }}>channels and develop a sales strategy based on this data.</p>
+                        <div>
+                            <CryptoCurrency data={filteredData} selectedCrypto={chooseCrypto} />
+                        </div>
                     </div>
                 </div>
 
@@ -53,14 +63,14 @@ const DataCryptos = () => {
                     <hr className="lying" />
                     <div className="cryptoContainer">
                         {filteredData.map((crypto) => (
-                            <div key={crypto.name} className="cryptos">
+                            <div key={crypto.name} className="cryptos" onClick={() => handleCryptoSelect(crypto)}>
                                 <img src={crypto.image} alt={crypto.name} />
                                 <div className="symbolYName">
                                     <p>{crypto.name}</p>
                                     <p>{crypto.symbol}</p>
                                 </div>
                                 <div className="price">
-                                <p>{crypto.current_price.toFixed(2)}</p>
+                                    <p>{crypto.current_price.toLocaleString(2)}</p>
                                 </div>
                             </div>
                         ))}
